2e4e9b57a21e61f232f56c32325d5848b309687e
Robbottone <gallu.roberto@gmail.com>
2024-12-04 16:48:45 +0100

    feat: first entities for ticketing

---
+++ GlobalTicket.TicketManagement.Domain/Entities/EventGig.cs
@@ -0,0 +1,11 @@
+﻿namespace GlobalTicket.TicketManagement.Domain.Entities;
+public class EventGig
+{
+	public Guid EventId       { get; set; }
+	public string Name        { get; set; } = string.Empty;
+	public string? Description { get; set; }
+	public int Price          { get; set; }
+	public string[] Artist    { get; set; } = [];
+	public DateTime Date      { get; set; }
+	public Guid CategoryId    { get; set; }
+}
---
+++ GlobalTicket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace GlobalTicket.TicketManagement.Domain.Entities;
+public class Order
+{
+	public Guid Id { get; set; }
+	public Guid UserId { get; set; }
+	public Guid EventId { get; set; }
+	public int TicketAmount { get; set; }
+	public DateTime OrderPlaced { get; set; }
+	public bool IsPaymentSuccessful { get; set; }
+}
