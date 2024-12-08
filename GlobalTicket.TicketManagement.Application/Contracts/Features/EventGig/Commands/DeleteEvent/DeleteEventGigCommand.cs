22246901f71997b5d450587133daa1fd30969509
Robbottone <gallu.roberto@gmail.com>
2024-12-08 15:23:39 +0100

    feat: delete command for event

---
+++ GlobalTicket.TicketManagement.Application/Contracts/Features/EventGig/Commands/DeleteEvent/DeleteEventGigCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+namespace GlobalTicket.TicketManagement.Application.Contracts.Features.EventGig.Commands.DeleteEvent;
+
+public class DeleteEventGigCommand: IRequest
+{
+	public Guid Id { get; set; }
+}
---
+++ GlobalTicket.TicketManagement.Application/Contracts/Features/EventGig/Commands/DeleteEvent/DeleteEventGigCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿
+using GlobalTicket.TicketManagement.Application.Contracts.Persistence;
+using MediatR;
+using EnsureThat;
+using System.Runtime.CompilerServices;
+
+namespace GlobalTicket.TicketManagement.Application.Contracts.Features.EventGig.Commands.DeleteEvent;
+
+public class DeleteEventGigCommandHandler : IRequestHandler<DeleteEventGigCommand>
+{
+	IEventGigRepostiory eventRepostiory { get; set; }
+	public DeleteEventGigCommandHandler(IEventGigRepostiory eventRepository)
+	{
+		this.eventRepostiory = eventRepository;
+	}
+
+	public async Task Handle(DeleteEventGigCommand request, CancellationToken cancellationToken)
+	{
+		EnsureArg.IsNotNull(request);
+		
+		var delete = await this.eventRepostiory.DeleteAsync(request.Id);
+	}
+}
