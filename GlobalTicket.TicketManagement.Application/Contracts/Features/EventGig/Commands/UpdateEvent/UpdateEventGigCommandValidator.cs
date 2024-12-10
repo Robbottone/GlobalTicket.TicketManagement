57fd4f51318ff792182128a0c50fc06b9973ad2f
Robbottone <gallu.roberto@gmail.com>
2024-12-10 16:22:36 +0100

    feat - written validator of updateventcommand

--- GlobalTicket.TicketManagement.Application/Contracts/Features/EventGig/Commands/UpdateEvent/UpdateEventGigCommandValidator.cs
+++ GlobalTicket.TicketManagement.Application/Contracts/Features/EventGig/Commands/UpdateEvent/UpdateEventGigCommandValidator.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
-namespace GlobalTicket.TicketManagement.Application.Contracts.Features.EventGig.Commands.UpdateEvent
+namespace GlobalTicket.TicketManagement.Application.Contracts.Features.EventGig.Commands.UpdateEvent;
+
+public class UpdateEventGigCommandValidator: AbstractValidator<UpdateEventGigCommand>
 {
-	internal class UpdateEventGigCommandValidator
-	{
+	public UpdateEventGigCommandValidator() {
+		RuleFor(p => p.Artist).NotEmpty().WithMessage("{propertyName} must contain at least of artist");
+		RuleFor(p => p.Price).GreaterThan(0).WithMessage("{propertyName} must be greater than 0");
+		RuleFor(p => p.Name).NotEmpty().WithMessage("{propertyName} must contain at least of name");
+		RuleFor(p => p.Description).NotEmpty().WithMessage("{propertyName} must contain at least the description");
+		RuleFor(p => p.EventDate).GreaterThan(new DateTime(1970,1,1));
 	}
 }
